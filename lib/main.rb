eb0eb36324c58bf9d7a6855a055feb2122be5517
Jonathan Salis <jtsseneca@hotmail.com>
2012-07-03 09:58:47 -0400

    Adding scope for planets with_my_ships
    A scope that joins planets with ships at the same coordinates
    and removing a create ship from main.rb as it's throwing an
    error with attack(ship_id).  TODO: check the error

--- lib/main.rb
+++ lib/main.rb
@@ -1,16 +1,6 @@
 raise "Username and Password Required" unless ARGV[0] && ARGV[1]
 load('config/initializers/schemaverse.rb')
 
-new_ship = MyShip.create!(
-  :name => "A New Ship",
-  :attack => 5,
-  :defense => 5,
-  :engineering => 5,
-  :prospecting => 5
-)
-
-#new_ship.upgrade('MAX_HEALTH', 5)
-
-MyShip.mine_all_planets
-
-raise Planet.my_planets.inspect
+Planet.with_my_ships.each do |planet|
+  puts planet.name
+end
--- lib/models/planet.rb
+++ lib/models/planet.rb
@@ -1,4 +1,6 @@
 class Planet < ActiveRecord::Base 
   self.primary_key = 'id'
+
   scope :my_planets, where(:conqueror_id => MyPlayer.first.id)
+  scope :with_my_ships, :joins => "INNER JOIN my_ships ON planets.location_x = my_ships.location_x AND planets.location_y = my_ships.location_y "
 end
