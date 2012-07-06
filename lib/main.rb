462406d78ac1afe5bcb30ce0920f51a3ca46c63c
Jonathan Salis <jtsseneca@hotmail.com>
2012-07-05 21:14:54 -0400

    Updating ship cost and upgrading cost and omitting sections
    The ship cost is not what was in create_schemaverse.sql but the
    value in the price_list table was 100.
    
    Omitting parts of the strat that aren't well thought out.

--- lib/main.rb
+++ lib/main.rb
@@ -15,8 +15,8 @@ Planet.my_planets.not_home.each do |planet|
 end
 
 if home.ships.size < home.mine_limit
-  ((my_player.balance + my_player.fuel_reserve) / 100000).times do
-    my_player.convert_fuel_to_money(100000) if my_player.balance < 10000
+  ((my_player.balance + my_player.fuel_reserve) / 1000).times do
+    my_player.convert_fuel_to_money(1000) if my_player.balance < 1000
 
     # If you can build another miner at home, do so
     miner = home.ships.create(:name => "#{home.name}-miner", :action => "MINE", :action_target_id => home.id)
@@ -29,37 +29,66 @@ else
   # If I have the same amount of miners on my home planet as the limit allows for, it makes more sense to upgrade the ships instead
   unless home.ships.average("prospecting+engineering+defense+attack") == max_ship_skill
     home.ships.where("(prospecting+engineering+defense+attack) < ?", max_ship_skill).each do |ship|
+      my_player.convert_fuel_to_money(5000) if my_player.balance < 5000
       skill_remaining = max_ship_skill - (ship.prospecting + ship.engineering + ship.defense + ship.attack)
       ship.upgrade('PROSPECTING', skill_remaining < 140 ? skill_remaining.to_i : 140)
     end
   end
 end
 
-home.ships.where("action IS NULL OR action = ''").each do |ship|
-  # These ships aren't mining our home and are extra, lets get them to explore
-  # Set them to explore
-  ship.update_attributes(:name => "#{USERNAME}-armada".capitalize, :action => 'ATTACK')
-  skill_remaining = max_ship_skill - (ship.prospecting + ship.engineering + ship.defense + ship.attack)
-  ship.upgrade('ATTACK', skill_remaining < 140 ? skill_remaining.to_i : 140)
-  ship.course_control(ship.max_speed, rand(360))
+# Build defenders for our home
+if home.ships.where("name LIKE ?", '%defender').size < 50
+  ((my_player.balance + my_player.fuel_reserve) / 1000).times do
+    my_player.convert_fuel_to_money(1000) if my_player.balance < 1000
+
+    # If you can build another miner at home, do so
+    miner = home.ships.create(:name => "#{home.name}-defender", :action => "ATTACK")
+
+    # Then upgrade it's prospecting
+    miner.upgrade('ATTACK', 240)
+    miner.upgrade('DEFENSE', 240)
+    break if home.ships.where("name LIKE ?", '%defender').count >= 50
+  end
 end
 
-MyShip.where("name LIKE ?", "%armada").each do |ship|
-  skill_remaining = max_ship_skill - (ship.prospecting + ship.engineering + ship.defense + ship.attack)
-  ship.upgrade('ATTACK', skill_remaining < 140 ? skill_remaining.to_i : 140)
-  if ship.planets_in_range.first && !Planet.my_planets.collect(&:id).include?(ship.planets_in_range.first.planet.id)
-    planet_in_range = ship.planets_in_range.first
 
-    unless planet_in_range.ship_location == planet_in_range.planet_location
-      # This isn't a planet that I have conquered yet, lets move to it'
-      ship.course_control(ship.max_speed, nil, "#{planet_in_range.planet_location[1..-1].chop}")
-    else
-      # The ship has arrived at the planet
-      ship.update_attributes(:action => "MINE", :action_target_id => planet_in_range.planet)
-      Planet.find(planet_in_range.planet).update_attribute("name", Planet.get_new_planet_name)
+# Start to build and mass armada ships at home
+#((my_player.balance + my_player.fuel_reserve) / 25000).times do
+#  my_player.convert_fuel_to_money(25000) if my_player.balance < 25000
+#  # If you can build another miner at home, do so
+#  ship = home.ships.create(:name => "#{USERNAME}-armada", :action => "ATTACK")
+#  ship.upgrade("ATTACK", 100)
+#  ship.upgrade("DEFENSE", 100)
+#end
+
+home.ships.where("name LIKE ?", "%armada").all.each_slice(80).to_a.each do |rogue_group|
+  if rogue_group.size == 80
+    direction = rand(360)
+    rogue_group.each do |rogue_ship|
+      rogue_ship.course_control(rogue_ship.max_speed / 2, direction)
+      new_name = MyShip.get_new_ship_name
+      rogue_ship.update_attribute("name", "#{new_name}")
     end
   end
 end
+
+
+#MyShip.where("name LIKE ?", "%armada").each do |ship|
+#  skill_remaining = max_ship_skill - (ship.prospecting + ship.engineering + ship.defense + ship.attack)
+#  ship.upgrade('ATTACK', skill_remaining < 140 ? skill_remaining.to_i : 140)
+#  if ship.planets_in_range.first && !Planet.my_planets.collect(&:id).include?(ship.planets_in_range.first.planet.id)
+#    planet_in_range = ship.planets_in_range.first
+#
+#    unless planet_in_range.ship_location == planet_in_range.planet_location
+#      # This isn't a planet that I have conquered yet, lets move to it'
+#      ship.course_control(ship.max_speed, nil, "#{planet_in_range.planet_location[1..-1].chop}")
+#    else
+#      # The ship has arrived at the planet
+#      ship.update_attributes(:action => "MINE", :action_target_id => planet_in_range.planet)
+#      Planet.find(planet_in_range.planet).update_attribute("name", Planet.get_new_planet_name)
+#    end
+#  end
+#end
 
 # Repair the ships
 MyShip.where("current_health = 0").each do |ship|
@@ -80,32 +109,15 @@ Planet.my_planets.not_home.each do |planet|
   end
 end
 
-# Build tons of ships and send them in all directions for now
-
-((my_player.balance + my_player.fuel_reserve) / 105000).times do
-  begin
-    my_player.convert_fuel_to_money(105000) if my_player.balance < 105000
-    # If you can build another miner at home, do so
-    ship = MyShip.create(:name => "#{USERNAME}-armada")
-    ship = ship.reload
-    ship.update_attribute("action", "ATTACK")
-    ship.upgrade("ATTACK", 100)
-    sleep(1)
-    ship.upgrade("DEFENSE", 100)
-    ship.course_control(ship.max_speed, rand(360))
-  rescue
-  end
-end
-
 puts "Round: #{TicSeq.first.last_value}"
 puts "#{USERNAME}: $#{my_player.balance}, #{my_player.fuel_reserve} fuel"
-roaming_ships = [MyShip.all]
+roaming_ships = MyShip.all
 Planet.my_planets.each do |planet|
   puts planet.name
   puts "Ships: #{planet.ships.size}"
   planet.ships.each do |ship|
     puts "     #{ship.name}: A: #{ship.attack}, D: #{ship.defense}, P: #{ship.prospecting}, E: #{ship.engineering}, ACTION: #{ship.action}"
-    roaming_ships = roaming_ships - ship
+    roaming_ships = roaming_ships - [ship]
   end
 end
 
--- lib/models/my_ship.rb
+++ lib/models/my_ship.rb
@@ -12,22 +12,34 @@ class MyShip < ActiveRecord::Base
     ActiveRecord::Base.connection.update_sql(sql)
   end
 
+  def self.get_new_ship_name
+    next_num = (MyShip.where("name LIKE ?", "%armada-%").size + 1).to_s
+    colour_codes = %w{ red orange yellow green blue indigo violet black brown}
+    n = "#{USERNAME}-armada-#{colour_codes[next_num[0].to_i - 1]}"
+    if next_num.size > 1
+      n += "-#{next_num[1..next_num.size]}"
+    else
+      n += "-LEADER"
+    end
+    n.capitalize
+  end
+
   def upgrade(attribute, amount)
     self.class.select("UPGRADE(#{self.id}, '#{attribute}', #{amount})").all
   end
-  
+
   def refuel_ship()
     self.class.select("REFUEL_SHIP(#{self.id})").all
   end
-  
+
   def commence_attack(ship_id)
     self.class.select("ATTACK(#{self.id}, #{ship_id})").all
-  end    
+  end
 
   def repair(ship_id)
     self.class.select("REPAIR(#{self.id}, #{ship_id})").all
   end
-  
+
   def mine(planet_id)
     self.class.select("MINE(#{self.id}, #{planet_id})").all
   end
@@ -37,5 +49,5 @@ class MyShip < ActiveRecord::Base
     dir = direction.nil? ? "NULL" : direction
     self.class.select("SHIP_COURSE_CONTROL(#{self.id}, #{speed}, #{dir}, #{dest})").all
   end
-  
+
 end
