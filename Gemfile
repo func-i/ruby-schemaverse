3b8a84e97a4e1e75bc7ef1291e2340bd5b734307
Abstrct <josh@schemaverse.com>
2012-07-07 13:58:55 -0400

    Changes to the way the database connection is made

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source :rubygems
 gem 'activerecord'
 gem 'psych'
 gem 'activerecord-postgresql-adapter'
+gem 'activerecord_url_connections'
--- config/initializers/schemaverse.rb
+++ config/initializers/schemaverse.rb
@@ -2,10 +2,8 @@ require 'rubygems'
 require 'active_record'
 require 'yaml'
 
-dbconfig = YAML::load(File.open('config/database.yml'))
 
-USERNAME, PASSWORD = ENV['SCHEMAVERSE_USERNAME'], ENV['SCHEMAVERSE_PASSWORD']
-ActiveRecord::Base.establish_connection(dbconfig.merge(:username => USERNAME, :password => PASSWORD))
+ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"])
 
 load('config/initializers/db.rb')
 load('lib/functions.rb')
