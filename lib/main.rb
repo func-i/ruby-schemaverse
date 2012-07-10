4f55f80fad16eacd3da4ee92f77a7eeefcf431c8
Jonathan Salis <jtsseneca@hotmail.com>
2012-07-10 11:44:01 -0400

    Requiring the schemaverse class
    require_relative on the schemaverse class in main

--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,6 @@
 raise "Username and Password Required" unless (ARGV[0] && ARGV[1]) || (ENV['DATABASE_URL'] && ENV['SCHEMAVERSE_USERNAME'])
 load('config/initializers/environment.rb')
+require_relative 'schemaverse'
 
 Schemaverse.new.play
 
